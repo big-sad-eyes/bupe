4af5ec9a310b11db72853bafd93b13942f7749b2
big-sad-eyes <big-sad-eyes@test.lan>
2017-04-08 17:43:51 -0400

    some work parsing real-world epubs. This may break builder (for now) but it returns more meaningful parse results for real epubs with unicode characters in metadata, with multiple identifiers, and with a description that has html in it

--- lib/bupe/parser.ex
+++ lib/bupe/parser.ex
@@ -74,13 +74,14 @@ defmodule BUPE.Parser do
     root_file = String.to_charlist(root_file)
     [{^root_file, content}] = extract_content(epub_file, [root_file])
 
-    {xml, _rest} = :xmerl_scan.string(String.to_charlist(content))
+    {xml, _rest} = :erlang.bitstring_to_list(content) |>  :xmerl_scan.string
 
     %BUPE.Config{
       title: find_metadata(xml, "title"),
       language: find_language(xml),
       version: find_version(xml),
-      identifier: find_metadata(xml, "identifier"),
+      #identifier: find_isbn(xml),
+      identifier: find_identifiers(xml),
       creator: find_metadata(xml, "creator"),
       contributor: find_metadata(xml, "contributor"),
       modified: find_modified(xml),
@@ -113,21 +114,22 @@ defmodule BUPE.Parser do
   end
 
   defp find_metadata(xml, meta) do
-    "/package/metadata/dc:#{meta}/text()"
+    "/package/metadata/dc:#{meta} | /package/metadata/#{meta}"
     |> xpath_string(xml)
-    |> parse_xml_text()
+    |> parse_record()
   end
+
 
   defp xpath_string(xpath, xml) do
     xpath
-    |> String.to_charlist()
+    |> :erlang.bitstring_to_list
     |> :xmerl_xpath.string(xml)
   end
 
   defp find_modified(xml) do
-    "/package/metadata/meta[contains(@property, 'dcterms:modified')]/text()"
+    "/package/metadata/meta[contains(@property, 'dcterms:modified')]"
     |> xpath_string(xml)
-    |> parse_xml_text()
+    |> parse_record()
   end
 
   defp find_version(xml) do
@@ -146,9 +148,91 @@ defmodule BUPE.Parser do
     |> parse_xml_attribute()
   end
 
-  defp parse_xml_text([{:xmlText, _parents, _pos, _language, value, :text}]), do: to_string(value)
-  defp parse_xml_text([]), do: nil
+  # example of more advanced xpath query -- not needed as just querying
+  # map returned by identifiers is better solution but this should be a good
+  # example of selecting by attribute values with xpath query
+  # 
+  #defp find_isbn(xml) do
+  #  "/package/metadata/dc:identifier[@*[. = 'ISBN'] | @*[. = 'isbn']]" 
+  #  |> xpath_string(xml)
+  #  |> parse_record()
+  #end
+
+
+  defp find_identifiers(xml) do
+    "/package/metadata/dc:identifier"
+    |> xpath_string(xml)
+    |> parse_identifiers()
+  end
+
+
+
+
+
+  defp parse_record([]), do: nil
+  defp parse_record([element]), do: combine_values(pr(element))
+  defp parse_record([head_element | tail_elements]), do: [ combine_values(pr([head_element])) | pr(tail_elements) ]
+
+
+  defp combine_values([]), do: nil
+  defp combine_values(values) when not is_list(values), do: values
+  defp combine_values(values) when     is_list(values), do: List.to_string( Enum.map(values,fn(v) -> to_string(v) end) )
+
+  
+  defp pr(nil), do: []
+  defp pr([]), do: []
+  defp pr([{:xmlElement, _name, _, _, _, _, _, _attributes, elements, _, _, _} | tail]), do: [ combine_values(pr(elements)) | pr(tail) ]
+  defp pr({:xmlElement, _name, _, _, _, _, _, _attributes, elements, _, _, _}), do: pr(elements)
+  defp pr([{:xmlComment, _, _, _, _value}]), do: []
+  defp pr({:xmlComment, _, _, _, _value}), do: []
+  defp pr([{:xmlText, _, _, _, value, _} | tail]), do: [to_string(value) | [combine_values(pr(tail))] ]
+  defp pr({:xmlText, _, _, _, value, _}), do: to_string(value)
+
 
   defp parse_xml_attribute([{:xmlAttribute, _name, _expanded_name, _nsinfo, _namespace, _parents, _pos, _language, value, _normalized}]), do: to_string(value)
   defp parse_xml_attribute([]), do: nil
+
+
+  defp parse_identifiers([]), do: nil
+  defp parse_identifiers([head_element | tail_elements]) do
+    Enum.map([head_element | tail_elements ], fn(e) -> parse_identifiers(e) end )
+    |> Enum.into(%{})
+  end
+  defp parse_identifiers({:xmlElement, _name, _, _, _, _, _, attributes, elements, _, _, _}) do
+    ident_id=parse_identifier_attributes(attributes)
+    if ident_id != nil do
+      id = elem(ident_id,1)
+      val = parse_record(elements)
+      {id, val}
+    else
+      nil
+    end
+  end
+
+
+  defp parse_identifier_attributes([head_element | tail_elements]) do
+    Enum.map([head_element | tail_elements ], fn(e) -> parse_identifier_attributes(e) end )
+    |> Enum.reduce(nil, fn(at,acc) -> combine_identifier_attributes(at,acc) end )                                    
+  end
+  defp parse_identifier_attributes( {:xmlAttribute, name, _expanded_name, _nsinfo, _namespace, _parents, _pos, _language, value, _normalized} ) do
+    nstr = to_string(name)
+    cond do
+      String.match?(nstr, ~r/:scheme$/) || String.match?(nstr, ~r/^scheme$/) ->
+        {:scheme, value}
+      String.match?(nstr, ~r/^id$/) ->
+        {:id, value}
+      true ->
+        nil
+    end
+  end
+  defp parse_identifier_attributes(nil), do: nil
+
+  defp combine_identifier_attributes({:id,value},_), do: {:id, value}
+  defp combine_identifier_attributes(_,{:id,value}), do: {:id, value}
+  defp combine_identifier_attributes(t,nil), do: t
+
+
 end
+
+
+
